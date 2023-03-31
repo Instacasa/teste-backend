be762610b59ce7f75dd7e6fb876db312d588baa2
Waldney Andrade <waldney.andrade@gmail.com>
2023-03-31 11:36:12 -0300

    Enhance usecase position to let test only testing

--- test/unit/useCases/post/getPost.test.ts
+++ test/unit/useCases/post/getPost.test.ts
@@ -6,11 +6,15 @@ import { PostModel, UserModel } from '@models';
 import { PostInterface, UserInterface } from '@types';
 
 describe('Get Post', () => {
-
-  let user: UserInterface;
+  let createPost: CreatePostUseCase;
+  let getPost: GetPostUseCase;
   let repository: PostRepository<PostInterface, PostModel>;
+  let user: UserInterface;
   beforeAll(async () => {
+    createPost = new CreatePostUseCase();
+    getPost = new GetPostUseCase();
     repository = new PostRepository<PostInterface, PostModel>();
+
     const userRepository = new UserRepository<UserInterface, UserModel>();
     user = new User({name: 'Admin', isAdmin: true});
     user.active = true;
@@ -22,8 +26,6 @@ describe('Get Post', () => {
   });
   
   test('Should get post', async () => {
-    const createPost = new CreatePostUseCase();
-    const getPost = new GetPostUseCase();
     const partialPost: Partial<PostInterface> = { title: 'Teste', text: 'Text text text', user };
     const post = await createPost.execute(user.id, partialPost);
     const gettedPost = await getPost.execute(post.id);
@@ -31,7 +33,6 @@ describe('Get Post', () => {
   });
 
   test('Shouldn\'t get inexistent post', async () => {
-    const getPost = new GetPostUseCase();
     await expect(() => 
       getPost.execute(0)
     ).rejects.toThrowError(new NotFoundError('post with id 0 can\'t be found.'));
