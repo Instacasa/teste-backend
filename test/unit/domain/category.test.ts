2927aefbf21f5b18dc4d353784044ec525a74962
Waldney Andrade <waldney.andrade@gmail.com>
2023-03-31 00:40:15 -0300

    Adding shouldn\'t create category without lable test

--- test/unit/domain/category.test.ts
+++ test/unit/domain/category.test.ts
@@ -1,9 +1,15 @@
 import { Category } from '@domains';
+import { ValidationError } from '@libs/errors';
 import { mockCategories } from '@mocks/category';
 
 describe('Category', () => {
   test('should create a valid category', () => {
     const [ category ] = mockCategories([{}]);
     expect(category).toBeInstanceOf(Category);
+  });
+
+  test('shouldn\'t create category without lable', () => {
+    expect(() => mockCategories([{label: ''}]))
+      .toThrowError(new ValidationError('O rótulo da categoria é obrigatório'));
   });
 });
