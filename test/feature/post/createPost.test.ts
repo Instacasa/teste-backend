694276b941eb63a3c0407e4c2648acd926fc74d6
Waldney Andrade <waldney.andrade@gmail.com>
2023-03-30 21:21:21 -0300

    Created 'Shouldn\'t create new post with inexistent user' test

--- test/feature/post/createPost.test.ts
+++ test/feature/post/createPost.test.ts
@@ -1,27 +1,31 @@
 import { PostRepository, UserRepository } from '@repositories';
-import { User } from '@domains';
 import { PostModel, UserModel } from '@models';
 import { PostInterface, UserInterface } from '@types';
 import httpStatus from 'http-status';
 import request from '../request';
+import { mockUsers } from '@mocks';
+import { faker } from '@faker-js/faker';
+
+const userRepository = new UserRepository<UserInterface, UserModel>();
+const postRepository = new PostRepository<PostInterface, PostModel>();
+
 
 describe('Post', () => {
 
   beforeEach(async () => {
-    const repository = new PostRepository<PostInterface, PostModel>();
-    await repository.deleteAll();
+    await postRepository.deleteAll();
+    await userRepository.deleteAll();
   });
 
   test('Should create new post', async () => {
-    const userRepository = new UserRepository<UserInterface, UserModel>();
-    let admin: UserInterface = new User({name: 'Admin', isAdmin: false});
+    let [ admin ]: UserInterface[] = mockUsers([{isAdmin: false}]);
     admin.active = true;
     admin = await userRepository.create(admin);
     const response = await request()
       .post(`/posts/user/${admin.id}`)
       .send({
         title: 'Teste post',
-        text: 'text text text'
+        text: faker.lorem.paragraph()
       })
       .expect(httpStatus.CREATED);
     
@@ -30,44 +34,52 @@ describe('Post', () => {
   });
 
   test('Shouldn\'t allow to create new post if user is inactive', async () => {
-    const userRepository = new UserRepository<UserInterface, UserModel>();
-    let admin: UserInterface = new User({name: 'Admin', isAdmin: false});
+    let [ admin ]: UserInterface[] = mockUsers([{isAdmin: false}]);
     admin = await userRepository.create(admin);
     const { body } = await request()
       .post(`/posts/user/${admin.id}`)
       .send({
-        title: 'Teste post',
-        text: 'text text text'
+        title: faker.lorem.sentence(),
+        text: faker.lorem.paragraph()
       })
       .expect(httpStatus.BAD_REQUEST);
     expect(body.error.name).toEqual('ValidationError');
   });
 
   test('Shouldn\'t create new post without title', async () => {
-    const userRepository = new UserRepository<UserInterface, UserModel>();
-    let admin: UserInterface = new User({name: 'Admin', isAdmin: false});
+    let [ admin ]: UserInterface[] = mockUsers([{isAdmin: false}]);
     admin.active = true;
     admin = await userRepository.create(admin);
     const { body } = await request()
       .post(`/posts/user/${admin.id}`)
       .send({
-        text: 'text text text'
+        text: faker.lorem.paragraph()
       })
       .expect(httpStatus.BAD_REQUEST);
     expect(body.error.name).toEqual('ValidationError');
   });
 
   test('Shouldn\'t create new post without text', async () => {
-    const userRepository = new UserRepository<UserInterface, UserModel>();
-    let admin: UserInterface = new User({name: 'Admin', isAdmin: false});
+    let [ admin ]: UserInterface[] = mockUsers([{isAdmin: false}]);
     admin.active = true;
     admin = await userRepository.create(admin);
     const { body } = await request()
       .post(`/posts/user/${admin.id}`)
       .send({
-        title: 'Teste post',
+        text: faker.lorem.paragraph()
       })
       .expect(httpStatus.BAD_REQUEST);
     expect(body.error.name).toEqual('ValidationError');
+  });
+
+  test('Shouldn\'t create new post with inexistent user', async () => {
+    const { body } = await request()
+      .post(`/posts/user/${+faker.random.numeric(6)}`)
+      .send({
+        title: faker.lorem.sentence(),
+        text: faker.lorem.paragraph()
+      })
+      .expect(httpStatus.NOT_FOUND);
+    expect(body.error.name).toEqual('NotFoundError');
   });
 });
