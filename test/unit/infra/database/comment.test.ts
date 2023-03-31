80ab679e595e75c17d8b555fe44f3c69393f5adb
Waldney Andrade <waldney.andrade@gmail.com>
2023-03-30 21:54:29 -0300

    Enhancing comment test

--- test/unit/infra/database/comment.test.ts
+++ test/unit/infra/database/comment.test.ts
@@ -1,29 +1,30 @@
 import { CommentRepository, UserRepository, PostRepository } from '@repositories';
 import { CommentInterface, PostInterface, UserInterface } from '@types';
 import { NotFoundError } from '@errors';
-import { User, Post, Comment } from '@domains';
 import { CommentModel, UserModel, PostModel } from '@models';
+import { mockComments, mockPosts, mockUsers } from '@mocks';
+
+
+const commentRepository = new CommentRepository<CommentInterface, CommentModel>();
+const userRepository = new UserRepository<UserInterface, UserModel>();
+const postRepository = new PostRepository<PostInterface, PostModel>();
 
 describe('Comment Repository', () => {
 
   beforeEach(async () => {
-    const repository = new CommentRepository<CommentInterface, CommentModel>();
-    await repository.deleteAll();
+    await commentRepository.deleteAll();
   });
 
   test('Should create new element on database', async () => {
-    let user: UserInterface = new User({ name: 'Test', isAdmin: true });
-    const userRepository = new UserRepository<UserInterface, UserModel>();
+    let [ user ]: UserInterface[] = mockUsers([{ isAdmin: true }]);
     user = await userRepository.create(user);
 
-    let post: PostInterface = new Post({title: 'Test', text: 'text test', user});
-    const postRepository = new PostRepository<PostInterface, PostModel>();
+    let [ post ]: PostInterface[] = mockPosts([{ user }]);
     post = await postRepository.create(post);
 
-    const comment = new Comment({text: 'text test', user, post});
-    const repository = new CommentRepository<CommentInterface, CommentModel>();
+    const [ comment ] = mockComments([{ user, post }]);
 
-    const newComment = await repository.create(comment);
+    const newComment = await commentRepository.create(comment);
     expect(newComment.id).toBeDefined();
     expect(newComment.id).toBeGreaterThan(0);
     expect(newComment.text).toEqual(comment.text);
@@ -32,20 +33,17 @@ describe('Comment Repository', () => {
   });
 
   test('Should update elemente on database', async () => {
-    let user: UserInterface = new User({ name: 'Test', isAdmin: true });
-    const userRepository = new UserRepository<UserInterface, UserModel>();
+    let [ user ]: UserInterface[] = mockUsers([{ isAdmin: true }]);
     user = await userRepository.create(user);
 
-    let post: PostInterface = new Post({title: 'Test', text: 'text test', user});
-    const postRepository = new PostRepository<PostInterface, PostModel>();
+    let [ post ]: PostInterface[] = mockPosts([{ user }]);
     post = await postRepository.create(post);
 
-    let comment: CommentInterface = new Comment({text: 'text test', user, post});
+    let [ comment ]: CommentInterface[] = mockComments([{ user, post }]);
 
-    const repository = new CommentRepository<CommentInterface, CommentModel>();
-    comment = await repository.create(comment);
+    comment = await commentRepository.create(comment);
     comment.text = 'Updated text';
-    const udpatedComment = await repository.update(comment);
+    const udpatedComment = await commentRepository.update(comment);
 
     expect(udpatedComment.id).toBeDefined();
     expect(udpatedComment.id).toBeGreaterThan(0);
@@ -53,67 +51,56 @@ describe('Comment Repository', () => {
   });
 
   test('Should get comment by id', async () => {
-    let user: UserInterface = new User({ name: 'Test', isAdmin: true });
-    const userRepository = new UserRepository<UserInterface, UserModel>();
+    let [ user ]: UserInterface[] = mockUsers([{ isAdmin: true }]);
     user = await userRepository.create(user);
 
-    let post: PostInterface = new Post({title: 'Test', text: 'text test', user});
-    const postRepository = new PostRepository<PostInterface, PostModel>();
+    let [ post ]: PostInterface[] = mockPosts([{ user }]);
     post = await postRepository.create(post);
 
-    let comment: CommentInterface = new Comment({text: 'text test', user, post});
+    let [ comment ]: CommentInterface[] = mockComments([{ user, post }]);
 
-    const repository = new CommentRepository<CommentInterface, CommentModel>();
-    comment = await repository.create(comment);
-    const persistedComment = await repository.get(comment.id);
+    comment = await commentRepository.create(comment);
+    const persistedComment = await commentRepository.get(comment.id);
     expect(persistedComment.id).toEqual(comment.id);
     expect(persistedComment.text).toEqual(comment.text);
   });
   
 
   test('Should get list of comment', async () => {
-    let user: UserInterface = new User({ name: 'Test', isAdmin: true });
-    const userRepository = new UserRepository<UserInterface, UserModel>();
+    let [ user ]: UserInterface[] = mockUsers([{ isAdmin: true }]);
     user = await userRepository.create(user);
 
-    let post: PostInterface = new Post({title: 'Test', text: 'text test', user});
-    const postRepository = new PostRepository<PostInterface, PostModel>();
+    let [ post ]: PostInterface[] = mockPosts([{ user }]);
     post = await postRepository.create(post);
 
-    let comment1: CommentInterface = new Comment({ text: 'text test', user, post });
-    let comment2: CommentInterface = new Comment({ text: 'text test', user, post });
-    let comment3: CommentInterface = new Comment({ text: 'text test', user, post });
+    let [ comment1, comment2, comment3 ]: CommentInterface[]
+      = mockComments([{ user, post }, { user, post }, { user, post }]);
 
-    const repository = new CommentRepository<CommentInterface, CommentModel>();
-    comment1 = await repository.create(comment1);
-    comment2 = await repository.create(comment2);
-    comment3 = await repository.create(comment3);
-    const persistedComment = await repository.list();
+    comment1 = await commentRepository.create(comment1);
+    comment2 = await commentRepository.create(comment2);
+    comment3 = await commentRepository.create(comment3);
+    const persistedComment = await commentRepository.list();
     expect(persistedComment).toHaveLength(3);
     expect(persistedComment[0].text).toEqual(comment3.text);
   });
   
 
   test('Should delete a comment', async () => {
-    let user: UserInterface = new User({ name: 'Test', isAdmin: true });
-    const userRepository = new UserRepository<UserInterface, UserModel>();
+    let [ user ]: UserInterface[] = mockUsers([{ isAdmin: true }]);
     user = await userRepository.create(user);
 
-    let post: PostInterface = new Post({title: 'Test', text: 'text test', user});
-    const postRepository = new PostRepository<PostInterface, PostModel>();
+    let [ post ]: PostInterface[] = mockPosts([{ user }]);
     post = await postRepository.create(post);
 
-    let comment1: CommentInterface = new Comment({ text: 'text test', user, post });
-    let comment2: CommentInterface = new Comment({ text: 'text test', user, post });
-    let comment3: CommentInterface = new Comment({ text: 'text test', user, post });
+    let [ comment1, comment2, comment3 ]: CommentInterface[]
+      = mockComments([{ user, post }, { user, post }, { user, post }]);
 
-    const repository = new CommentRepository<CommentInterface, CommentModel>();
-    comment1 = await repository.create(comment1);
-    comment2 = await repository.create(comment2);
-    comment3 = await repository.create(comment3);
-    await repository.delete(comment2.id);
+    comment1 = await commentRepository.create(comment1);
+    comment2 = await commentRepository.create(comment2);
+    comment3 = await commentRepository.create(comment3);
+    await commentRepository.delete(comment2.id);
     try {
-      await repository.get(comment2.id);
+      await commentRepository.get(comment2.id);
     } catch(error) {
       expect(error as Error).toBeInstanceOf(NotFoundError);
     }
