cd738385569fdbc55cda71aaf1547c1e95161356
Waldney Andrade <waldney.andrade@gmail.com>
2023-03-31 12:54:03 -0300

    Setting admin user active

--- test/unit/useCases/catetory/createCategory.test.ts
+++ test/unit/useCases/catetory/createCategory.test.ts
@@ -14,6 +14,7 @@ describe('Create Category', () => {
     createCategory = new CreateCategoryUseCase();
     userRepository = new UserRepository<UserInterface, UserModel>();
     user = new User({ name: 'Admin', isAdmin: true });
+    user.active = true;
     user = await userRepository.create(user);
   });
 
