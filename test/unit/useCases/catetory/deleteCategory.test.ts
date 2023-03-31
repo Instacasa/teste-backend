b6bd184e1045d20aae9477ebe82350effff15cd2
Waldney Andrade <waldney.andrade@gmail.com>
2023-03-31 13:18:50 -0300

    Create 'Shouldn\'t delete category if user isn\'t admin' TEST

--- test/unit/useCases/catetory/deleteCategory.test.ts
+++ test/unit/useCases/catetory/deleteCategory.test.ts
@@ -1,16 +1,22 @@
 import { User } from '@domains';
 import { faker } from '@faker-js/faker';
-import { NotFoundError } from '@errors';
+import { NotFoundError, ValidationError } from '@errors';
 import { CategoryModel, UserModel } from '@models';
 import { CategoryRepository, UserRepository } from '@repositories';
 import { CategoryInterface, UserInterface } from '@types';
 import { CreateCategoryUseCase, DeleteCategoryUseCase, GetCategoryUseCase } from '@useCases';
 
 describe('Delete Category', () => {
+  let createCategory: CreateCategoryUseCase;
+  let deleteCategory: DeleteCategoryUseCase;
+  let getCategory: GetCategoryUseCase;
   let user: UserInterface;
   let userRepository: UserRepository<UserInterface, UserModel>;
   let categoryRepository: CategoryRepository<CategoryInterface, CategoryModel>;
   beforeAll(async () => {
+    createCategory = new CreateCategoryUseCase();
+    deleteCategory = new DeleteCategoryUseCase();
+    getCategory = new GetCategoryUseCase();
     categoryRepository = new CategoryRepository<CategoryInterface, CategoryModel>();
     userRepository = new UserRepository<UserInterface, UserModel>();
     user = new User({ name: 'Admin', isAdmin: true });
@@ -23,9 +29,6 @@ describe('Delete Category', () => {
   });
 
   test('Should delete category if user is the admin', async () => {
-    const createCategory = new CreateCategoryUseCase();
-    const deleteCategory = new DeleteCategoryUseCase();
-    const getCategory = new GetCategoryUseCase();
     const partialCategory: Partial<CategoryInterface> = { label: faker.lorem.word() };
     const category = await createCategory.execute(user.id, partialCategory);
     await deleteCategory.execute(user.id, category.id);
@@ -34,4 +37,12 @@ describe('Delete Category', () => {
     ).rejects.toThrowError(new NotFoundError(`category with id ${category.id} can't be found.`));
   });
 
+  test('Shouldn\'t delete category if user isn\'t admin', async () => {
+    const simpleUser = await userRepository.create(new User({name: 'Simple user', isAdmin: false}));
+    const partialCategory: Partial<CategoryInterface> = { label: faker.lorem.word() };
+    const category = await createCategory.execute(user.id, partialCategory);
+    await expect(() => 
+      deleteCategory.execute(simpleUser.id, category.id)
+    ).rejects.toThrowError(new ValidationError('Apenas administradores podem excluir categorias'));
+  });
 });
