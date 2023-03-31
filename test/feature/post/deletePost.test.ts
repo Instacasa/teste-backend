a50b79ae69d4d72673245f35d53a089314d10145
Waldney Andrade <waldney.andrade@gmail.com>
2023-03-31 16:25:19 -0300

    Enhance deletePost test

--- test/feature/post/deletePost.test.ts
+++ test/feature/post/deletePost.test.ts
@@ -1,51 +1,50 @@
 import { PostRepository, UserRepository } from '@repositories';
-import { Post, User } from '@domains';
+import { User } from '@domains';
 import { PostModel, UserModel } from '@models';
 import { PostInterface, UserInterface } from '@types';
 import httpStatus from 'http-status';
 import request from '../request';
+import { mockPosts } from '@mocks';
 
 describe('Post', () => {
+  let userRepository: UserRepository<UserInterface, UserModel>;
+  let repository: PostRepository<PostInterface, PostModel>;
+  let admin: UserInterface;
+  let user: UserInterface;
+  beforeAll(async () => {
+    userRepository = new UserRepository<UserInterface, UserModel>();
+    repository = new PostRepository<PostInterface, PostModel>();
 
-  beforeEach(async () => {
-    const repository = new PostRepository<PostInterface, PostModel>();
-    await repository.deleteAll();
-  });
-  
-  test('Should delete post by admins', async () => {
-    const userRepository = new UserRepository<UserInterface, UserModel>();
     let admin: UserInterface = new User({name: 'Admin', isAdmin: true});
     admin.active = true;
     admin = await userRepository.create(admin);
-    let user: UserInterface = new User({name: 'Admin', isAdmin: true});
+
+    let user: UserInterface = new User({name: 'Simple user', isAdmin: false});
     user.active = true;
     user = await userRepository.create(user);
-    const postRepository = new PostRepository<PostInterface, PostModel>();
-    let post: PostInterface = new Post({title: 'Post', text: 'Text text text', user});
-    post = await postRepository.create(post);
-    const { body } = await request()
+  });
+
+  beforeEach(async () => {
+    await repository.deleteAll();
+  });
+  
+  test('Should delete post by admins', async () => {    
+    let [post]: PostInterface[] = mockPosts([{user}]);
+    post = await repository.create(post);
+    await request()
       .delete(`/posts/${post.id}/user/${admin.id}`)
       .expect(httpStatus.ACCEPTED);
   });
   
   test('Should delete post by owner', async () => {
-    const userRepository = new UserRepository<UserInterface, UserModel>();
-    let user: UserInterface = new User({name: 'Admin', isAdmin: true});
-    user.active = true;
-    user = await userRepository.create(user);
-    const postRepository = new PostRepository<PostInterface, PostModel>();
-    let post: PostInterface = new Post({title: 'Post', text: 'Text text text', user});
-    post = await postRepository.create(post);
-    const { body } = await request()
+    let [post]: PostInterface[] = mockPosts([{user}]);
+    post = await repository.create(post);
+    await request()
       .delete(`/posts/${post.id}/user/${user.id}`)
       .expect(httpStatus.ACCEPTED);
   });
 
   test('Shouldn\'t delete inexistent post', async () => {
-    const userRepository = new UserRepository<UserInterface, UserModel>();
-    let user: UserInterface = new User({name: 'Admin', isAdmin: true});
-    user.active = true;
-    user = await userRepository.create(user);
     const { body } = await request()
       .delete(`/posts/0/user/${user.id}`)
       .expect(httpStatus.NOT_FOUND);
@@ -53,16 +52,8 @@ describe('Post', () => {
   });
 
   test('Shouldn\'t allow to delete post without been an admin or owner', async () => {
-    const userRepository = new UserRepository<UserInterface, UserModel>();
-    let admin: UserInterface = new User({name: 'Admin', isAdmin: true});
-    admin.active = true;
-    admin = await userRepository.create(admin);
-    let user: UserInterface = new User({name: 'Admin', isAdmin: false});
-    user.active = true;
-    user = await userRepository.create(user);
-    const postRepository = new PostRepository<PostInterface, PostModel>();
-    let post: PostInterface = new Post({title: 'Post', text: 'Text text text', user: admin});
-    post = await postRepository.create(post);
+    let [post]: PostInterface[] = mockPosts([{user: admin}]);
+    post = await repository.create(post);
     const { body } = await request()
       .delete(`/posts/${post.id}/user/${user.id}`)
       .expect(httpStatus.BAD_REQUEST);
